88319dac85b4dff50d9cc12924e1d9e226d97d23
Bruno <129236021+Bruno-T-S@users.noreply.github.com>
2024-10-11 11:03:23 -0300

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,1 +1,10 @@
+class Usuario:
+    def __init__(self, usuario, senha):
+        self.usuario = usuario
+        self.senha = senha
+
+def add_usuario(lista_usuarios, user):
+    lista_usuarios.append(user)
+
+lista_usuarios = []
 
