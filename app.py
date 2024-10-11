88b83d5892fd3179117feda62b6ff4cce48d4a1f
Bruno <129236021+Bruno-T-S@users.noreply.github.com>
2024-10-11 11:39:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+from controllers.usuario_controller import usuarios_controller
+
+app = Flask(__name__)
+app.register_blueprint(usuarios_controller)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
