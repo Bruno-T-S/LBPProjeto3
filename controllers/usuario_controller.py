40a8a1a6a8373a6b96bca14a52a9bbf9e2787b57
Bruno <129236021+Bruno-T-S@users.noreply.github.com>
2024-10-18 11:27:24 -0300

    Update usuario_controller.py

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -1,4 +1,5 @@
 from models.usuario_model import usuarios, addUsuario
+from controllers.cadastro_controller import *
 from flask import Blueprint, render_template, request, redirect, url_for, session
 usuarios_controller = Blueprint('usuario', __name__)
 
@@ -7,15 +8,32 @@ usuarios_controller.secret_key = 'chave_muito_segura'
 @usuarios_controller.route('/')
 def index():
     if 'login' in session:
-        return render_template('index.html', usuarios=usuarios)
-    return 'Você não está logado! Faça <a href="/login">login</a> ou então <a href="cadastrar">crie sua conta</a>'
+        return render_template('logado', usuarios=usuarios)
+    return render_template('home.html')
+
+@usuarios_controller.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        login = request.form.get('login')
+        senha = request.form.get('senha')
+        for usuario in usuarios:
+            if login == usuario.login and senha == usuario.senha:
+                return f'Acesso autenticado, {login}'
+        return f'Acesso negado. Clique <a href="/" para voltar para a página inicial.>aqui</a>'
+    return render_template('login.html')
 
 @usuarios_controller.route("/cadastrar", methods=["POST", "GET"])
 def add():
-    login = request.form['login']
-    senha = request.form['senha']
-    addUsuario(login, senha)
-    return redirect(url_for('usuario.index'))
+    if request.method == "POST":
+        login = request.form.get('login')
+        senha = request.form.get('senha')
+        confirmacao = request.form.get('confirmacao')
+        if not False in (validarSenha(senha, confirmacao) or validarLogin(login)):
+            addUsuario(login, senha)
+            return redirect(url_for('usuario.index'))
+        else:
+            return "Erro no cadastro."
+    return render_template('cadastrar.html')
 
 @usuarios_controller.route('/logout')
 def logout():
