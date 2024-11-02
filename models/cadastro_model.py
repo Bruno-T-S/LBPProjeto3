9a3fbe296988954413e4b9a5677ff9038537a547
Bruno <129236021+Bruno-T-S@users.noreply.github.com>
2024-11-01 23:21:55 -0300

    Envio final

---
+++ models/cadastro_model.py
@@ -0,0 +1,37 @@
+import string
+maiusculas = list(string.ascii_uppercase)
+numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+especiais = list(string.punctuation)
+def validarSenha(senha, senha_validacao, maiusculas=maiusculas, numeros=numeros, especiais=especiais):
+    tem_maiuscula = False
+    tem_numero = False
+    tem_especial = False
+    igual_validacao = False
+    tamanho_correto = False
+    nao_vazio = True
+
+    if senha == "" or senha_validacao == "":
+        nao_vazio = False
+    if senha == senha_validacao:
+        igual_validacao = True
+
+    if len(senha) >= 8 and len(senha) <=64:
+        tamanho_correto = True
+    for char in senha:
+        if char in maiusculas:
+            tem_maiuscula = True
+        if char in numeros:
+            tem_numero = True
+        if char in especiais:
+            tem_especial = True
+
+    return nao_vazio, tamanho_correto, igual_validacao, tem_maiuscula, tem_numero, tem_especial
+
+def validarLogin(nome):
+    nao_vazio = True
+    tamanho = False
+    if nome == "":
+        nao_vazio = False
+    if len(nome) >= 3:
+        tamanho = True
+    return nao_vazio, tamanho
--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,15 +1,17 @@
 class Usuario:
-    def __init__(self, id, login, senha):
+    def __init__(self, id, login, senha, admin):
         self.id = id
         self.login = login
         self.senha = senha
-
+        self.admin = admin
 usuarios = []
 
-def addUsuario(login, senha):
+def addUsuario(login, senha, admin=False):
     for usuario in usuarios:
         if usuario.login == login:
             return False
-    id = len(usuarios) + 1
-    usuarios.append(Usuario(id, login, senha))
+    id = len(usuarios)
+    usuarios.append(Usuario(id, login, senha, admin))
     return True  
+
+addUsuario("adm", "senha", True)
